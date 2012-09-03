79ff55b3fe6a6f5a951db515b5d9a8b7767f700d
Øyvind Robertsen <oyvind@arcticstyle.net>
2012-09-03 22:42:13 +0200

    improved formatting

--- Oving1/oppg1.py
+++ Oving1/oppg1.py
@@ -1,5 +1,6 @@
 ################################
 ## Øving 1 - TDT4110
+## Oppgave 1
 ## Converting polar coordinates
 ## to carthesian coordinates.
 ## Øyvind Robertsen
@@ -10,21 +11,21 @@ pi = 3.1415
 
 r = 3
 theta = pi/2
-x = r * math.cos(theta)
-y = r * math.sin(theta)
+x = format(r * math.cos(theta), '.5f')
+y = format(r * math.sin(theta), '.5f')
 
 print('The carthesian equivalent of ( 3 , pi/2 ) is (',x,',',y,')')
 
 r = 2.3
 theta = pi/3
-x = r * math.cos(theta)
-y = r * math.sin(theta)
+x = format(r * math.cos(theta), '.5f')
+y = format(r * math.sin(theta), '.5f')
 
 print('The carthesian equivalent of ( 2.3 , pi/3 ) is (',x,',',y,')')
 
 r = 5
 theta = 0
-x = r * math.cos(theta)
-y = r * math.sin(theta)
+x = format(r * math.cos(theta), '.5f')
+y = format(r * math.sin(theta), '.5f')
 
 print('The carthesian equivalent of ( 5 , 0 ) is (',x,',',y,')')
