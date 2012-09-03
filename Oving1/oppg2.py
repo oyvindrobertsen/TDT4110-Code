81a3fd9d760d8bf9acf14137f118e29412f8bcb9
Øyvind Robertsen <oyvind@arcticstyle.net>
2012-09-03 23:02:44 +0200

    added oppg2.py, remember to do test for correct binomial formula

---
+++ Oving1/oppg2.py
@@ -0,0 +1,11 @@
+################################
+## Øving 1 - TDT4110
+## The probability of getting
+## two pair in Texas Hold'em
+## Øyvind Robertsen
+################################
+import math
+
+coeff1 = (13 * (13-1))/(2 * 1)
+coeff2 = pow(((4 * (4-1))/(2 * 1)),2)
+coeff3 = (11 * (11-1)
