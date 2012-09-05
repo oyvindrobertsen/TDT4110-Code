ebf13e514fde81bd61ebbc7a5bab8ea9d501821b
Øyvind Robertsen <oyvind@arcticstyle.net>
2012-09-05 12:46:07 +0200

    finished oppg3.py

---
+++ Oving1/oppg3.py
@@ -0,0 +1,19 @@
+################################
+## Øving 1 - TDT4110
+## Oppgave 3
+## The probability of getting
+## two pair in Texas Hold'em
+## Øyvind Robertsen
+################################
+
+import math
+
+# a)
+
+pi = format(4 * math.atan(1), '.5f')
+print('The value of pi is: ',pi,'.',sep='')
+
+# b)
+
+root = format(math.sqrt(2), '.5f')
+print('The square root of 2 is: ',root,'.',sep='')
