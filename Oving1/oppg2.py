32a2ac4ab8b6f0270feba9eff510d63fcd0b3f4e
Øyvind Robertsen <oyvind@arcticstyle.net>
2012-09-05 12:17:30 +0200

    added oppg2.py

--- Oving1/oppg2.py
+++ Oving1/oppg2.py
@@ -8,4 +8,10 @@ import math
 
 coeff1 = (13 * (13-1))/(2 * 1)
 coeff2 = pow(((4 * (4-1))/(2 * 1)),2)
-coeff3 = (11 * (11-1)
+coeff3 = 11/1
+coeff4 = 4/1
+
+favored_possibilities = coeff1 * coeff2 * coeff3 * coeff4
+total_possibilities = 2598960
+probability_of_two_pair= format(favored_possibilities/total_possibilities, '.4f')
+print('The probability of getting two pair in Texas Hold\'em is ',probability_of_two_pair,'.',sep='')
