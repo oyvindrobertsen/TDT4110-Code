70139bb452587bf81d14881d6eb34c69ee4b880f
Øyvind Robertsen <oyvind@arcticstyle.net>
2012-09-07 12:24:43 +0200

    added a function

--- measurements.py
+++ measurements.py
@@ -3,3 +3,12 @@ maling2 = float(input('Andre måling: '))
 maling3 = float(input('Tredje måling: '))
 snitt = format((maling1+maling2+maling3)/3, '.4f')
 print('Snittet av målingene:', snitt,'.')
+
+
+def add(a, b):
+    c = a + b
+    return c
+
+p = add(2, 4)
+
+print('The value of p is',p,)
