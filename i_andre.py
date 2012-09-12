1829d1ae4cfe630c4a38b56fb5383986e1a2a15f
Øyvind Robertsen <oyvind@arcticstyle.net>
2012-09-12 09:41:05 +0200

    added multi.py and i_andre.py

---
+++ i_andre.py
@@ -0,0 +1,7 @@
+def i_andre():
+    tall = input('Tast inn tallet som skal kvadreres: ')
+    tall_int = int(tall)
+    tall2 = tall_int * tall_int
+    print(tall2)
+
+i_andre()
---
+++ multi.py
@@ -0,0 +1,15 @@
+def main():
+    multi_to()
+    divi_to()
+
+def multi_to():
+    tall = eval(input('Skriv inn et tall du vil multiplisere med to: '))
+    tall_multi = tall * 2
+    print (tall_multi)
+
+def divi_to():
+    tall = eval(input('Skriv inn et tall du vil dividere med to: '))
+    tall_divi = tall / 2
+    print (tall_divi)
+
+main()
